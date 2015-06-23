d431ae968fc6a4f1b2148742d18fea3ddf27d76c
antonior <antonio.rueda-martin@genomicsengland.co.uk>
2015-06-23 17:38:41 +0100

    Fixed compilation errors

--- schemas/converters.py
+++ schemas/converters.py
@@ -5,14 +5,14 @@ __author__ = 'antonior'
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 os.system("java -jar " + os.path.join(BASE_DIR, "resources", "avro-tools-1.7.7.jar") + " idl2schemata " +
-                  os.path.join(BASE_DIR, "schemas", "IDLs", "InterpretationRequestRD.avdl") + " "
-                  + os.path.join(BASE_DIR, "schemas", "JSONs", "ReportTriggeringRD"))
+                  os.path.join(BASE_DIR, "schemas", "IDLs", "CancerParticipant.avdl") + " "
+                  + os.path.join(BASE_DIR, "schemas", "JSONs", "CancerParticipant"))
 os.system("java -jar " + os.path.join(BASE_DIR, "resources", "avro-tools-1.7.7.jar") + " idl2schemata " +
                   os.path.join(BASE_DIR, "schemas", "IDLs", "InterpretationRequestRD.avdl") + " "
                   + os.path.join(BASE_DIR, "schemas", "JSONs", "ReportTriggeringRD"))
 os.system("java -jar " + os.path.join(BASE_DIR, "resources", "avro-tools-1.7.7.jar") + " idl2schemata " +
-                  os.path.join(BASE_DIR, "schemas", "IDLs", "InterpretationRequestRD.avdl") + " "
-                  + os.path.join(BASE_DIR, "schemas", "JSONs", "ReportTriggeringRD"))
+                  os.path.join(BASE_DIR, "schemas", "IDLs", "InterpretedGenomesRD.avdl") + " "
+                  + os.path.join(BASE_DIR, "schemas", "JSONs", "InterpretedGenomesRD"))
 os.system("java -jar " + os.path.join(BASE_DIR, "resources", "avro-tools-1.7.7.jar") + " idl2schemata " +
-                  os.path.join(BASE_DIR, "schemas", "IDLs", "InterpretationRequestRD.avdl") + " "
-                  + os.path.join(BASE_DIR, "schemas", "JSONs", "ReportTriggeringRD"))
+                  os.path.join(BASE_DIR, "schemas", "IDLs", "RDParticipant.avdl") + " "
+                  + os.path.join(BASE_DIR, "schemas", "JSONs", "RDParticipant"))
