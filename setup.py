09f1575c4de5288100696634a031bbe8b7e352d4
antonior <antonio.rueda-martin@genomicsengland.co.uk>
2016-01-06 18:05:57 +0000

    Add dependencies, change the version in the Setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from distutils.core import setup
-
+from pip.req import parse_requirements
+install_reqs = parse_requirements("requirements.txt")
+reqs = [str(ir.req) for ir in install_reqs]
 setup(
     name='GelReportModels',
-    version='v0.2.0',
+    version='v1.1.1',
     packages=['protocols'],
     url='',
     license='',
     author='Bioinformatics Team at Genomics England',
     author_email='antonio.rueda-martin@genomicsengland.co.uk',
-    description='Bioinformatics team model definitions', requires=['avro']
+    description='Bioinformatics team model definitions',
+    install_requires=reqs,
 )
