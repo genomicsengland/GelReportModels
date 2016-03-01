bd2f6eb1a94facc058bfa4a10357db9d36051436
antonior <antonio.rueda-martin@genomicsengland.co.uk>
2016-03-01 13:08:02 +0000

    Updated changes and version

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import json
 from distutils.core import setup
 import os
 from pip.req import parse_requirements
@@ -5,9 +6,12 @@ import uuid
 
 install_reqs = parse_requirements(os.path.join(os.path.dirname(__file__), "requirements.txt"), session=uuid.uuid1())
 reqs = [str(ir.req) for ir in install_reqs if ir.req is not None]
+
+BASE_DIR = os.path.dirname(__file__)
+VERSION = json.load(open(os.path.join(BASE_DIR, "schemas", "JSONs", "VersionControl", "VersionControl.avsc")))["fields"][0]["default"]
 setup(
     name='GelReportModels',
-    version='1.1.4',
+    version=VERSION,
     packages=['protocols'],
     url='',
     license='',
