f775050ca059c029c36ed95ec0925af7f4eb0d2b
priesgo <pablo.ferreiro@genomicsengland.co.uk>
2017-01-13 09:47:47 +0000

    changing licence to GEL

--- src/test/java/org/gel/cva/models/dto/KnownVariantTest.java
+++ src/test/java/org/gel/cva/models/dto/KnownVariantTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 OpenCB
+ * Copyright 2017 Genomics England Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.gel.cva.models.dto;
 
 import org.junit.Before;
