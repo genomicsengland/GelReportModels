bcdc3c9aab303e8bfb4cb2e8c0b17163e659764a
priesgo <pablo.ferreiro@genomicsengland.co.uk>
2017-05-16 16:02:27 +0100

    refactoring source code generation

--- README.md
+++ README.md
@@ -3,15 +3,20 @@
 
 This project contains several models used by GEL. These models are defined using Avro Interface Design Language (IDL) which is agnostic of any implementation language. The models are then used to generate source code employed to store the information. The source code is either Python or Java so far, but this can be easily extended.
 
-## Java source code
+From the Avro models you can generate:
+* Java source code
+* Python source code
+* JSON schemas
+* AVPR schemas
+* HTML documentation
 
 Maven is employed to manage the source code generation and dependency management.
 
-To generate sources run: 
+To generate sources and documentation run: 
 ```
-% mvn generate-sources
+% mvn clean generate-sources
 ```
-This will create a set of classes representing the Avro records in the folder `./target/generated-sources/avro`.
+This will create a set of Java classes representing the Avro records in the folder `./target/generated-sources/avro`. A set of Python classes under `./protocols`. The models documentation under `./docs/html_schemas/latest`
 
 
 To pack the Java source code representing these models in a jar file use:
@@ -19,7 +24,7 @@ To pack the Java source code representing these models in a jar file use:
 % mvn package
 ```
 
-To install it in your system so it is accesible as a maven dependency to other applications run:
+To install it in your system so it is accessible as a maven dependency to other applications run:
 ```
 % mvn install
 ```
@@ -28,20 +33,13 @@ To create a war file containing the HTML documentation for the models and the mo
 ```
 % mvn package -Dp.type=war
 ```
+This war can be deployed as a documentation service.
 
 
-### OpenCB dependencies
+## OpenCB dependencies
 
 The CVA model is extending the OpenCB variant model. In order to do so we need some Avro definitions from OpenCB biodata-models. Maven is extracting the required files from the biodata-models.jar file and use them to generate the required sources.
 
-## Model documentation
+## Dependencies
 
-The model documentation is at `./src/main/html/model-documentation.html`. This documentation has been generated with `avrodoc` (https://github.com/ept/avrodoc). To regenerate it run:
-```
-# creates *.avsc files from *.avdl
-% mvn generate-sources
-# generates html from the schemas
-% avrodoc target/generated-sources/avsc/* > ./src/main/html/model-documentation.html
-```
-
-**NOTE**: to install avrodoc run `npm install avrodoc -g`
+**Avrodoc**: to install avrodoc run `npm install avrodoc -g`
--- build.py
+++ build.py
@@ -1,10 +1,12 @@
 import json
 import os
-
 import sys
 import fnmatch
+import logging
 
 __author__ = 'antonior'
+
+logging.basicConfig(level=logging.INFO)
 
 BASE_DIR = os.path.dirname(__file__)
 
@@ -15,19 +17,6 @@ else:
 
 module_version = v.replace('.', '_')
 
-schemas = os.path.join(BASE_DIR, "schemas/IDLs", v, "org.gel.models.report.avro")
-ga4gh_schemas = os.path.join(BASE_DIR, "schemas/IDLs", v, "org.ga4gh.models")
-openCB_schemas = os.path.join(BASE_DIR, "schemas/IDLs", v, "org.opencb.biodata.models.sequence")
-cva_schemas = os.path.join(BASE_DIR, "schemas/IDLs", v, "org.gel.models.cva.avro")
-if v == 'latest':
-    outfile = os.path.join(BASE_DIR, "protocols", "GelProtocols.py")
-else:
-    outfile = os.path.join(BASE_DIR, "protocols", "GelProtocols_{version}.py".format(version=module_version))
-
-
-ga4gh_outfile = os.path.join(BASE_DIR, "protocols", "GA4GHProtocols.py")
-openCB_outfile = os.path.join(BASE_DIR, "protocols", "openCBProtocols.py")
-cva_outfile = os.path.join(BASE_DIR, "protocols", "CVAProtocols.py")
 avro_tools_jar = os.path.join(BASE_DIR, "resources", "bin", "avro-tools-1.7.7.jar")
 json_folder = os.path.join(BASE_DIR, "schemas", "JSONs", v)
 avrp_folder = os.path.join(BASE_DIR, "schemas", "AVPRs", v)
@@ -48,54 +37,90 @@ idls = [os.path.join(dirpath, f)
         for dirpath, dirnames, files in os.walk(idl_base_path)
         for f in fnmatch.filter(files, '*.avdl')]
 
+logging.info("Transforming AVDL schema to other representations...")
 for idl in idls:
-    print "transforming: " + idl
+    logging.info("Transforming: " + idl)
     base = os.path.basename(idl).replace(".avdl", "")
-    os.system("java -jar " + avro_tools_jar + " idl2schemata " + idl + " " + os.path.join(json_folder, base))
-    os.system("java -jar " + avro_tools_jar + " idl " + idl + " " + os.path.join(avrp_folder, base + ".avpr"))
+    idl2schemata_command = "java -jar " + avro_tools_jar + " idl2schemata " + idl + " " + \
+                           os.path.join(json_folder, base)
+    logging.info("Running: [%s]" % idl2schemata_command)
+    os.system(idl2schemata_command)
+    idl_command = "java -jar " + avro_tools_jar + " idl " + idl + " " + os.path.join(avrp_folder, base + ".avpr")
+    logging.info("Running: [%s]" % idl_command)
+    os.system(idl_command)
+
+def generate_python_sources(schema, source, version):
+    """
+    Generating Python source code from an AVRO schema
+    :param schema: the avro schema
+    :param source: the source to be generated
+    :param version: the version
+    :return:
+    """
+    logging.info("Version: " + version)
+    # GeL models Python source generation
+    source_generation_command = "python " + os.path.join(BASE_DIR, "resources", "CodeGenerationFromGA4GH",
+                                                         "process_schemas.py --outputFile "
+                                                  + source + " --avro-tools-jar " + avro_tools_jar +
+                                                         " --inputSchemasDirectory "
+                                                  + schema + " " + version + " --verbose    ")
+    logging.info(source_generation_command)
+    os.system(source_generation_command)
 
 
-VERSION = json.load(open(os.path.join(json_folder, "VersionControl", "VersionControl.avsc")))["fields"][0]["default"]
+logging.info("Generating Python source code from schemas...")
+schemas = os.path.join(BASE_DIR, "schemas/IDLs", v, "org.gel.models.report.avro")
+ga4gh_schemas = os.path.join(BASE_DIR, "schemas/IDLs", v, "org.ga4gh.models")
+openCB_schemas = os.path.join(BASE_DIR, "schemas/IDLs", v, "org.opencb.biodata.models.sequence")
+cva_schemas = os.path.join(BASE_DIR, "schemas/IDLs", v, "org.gel.models.cva.avro")
+ontologies_schemas = os.path.join(BASE_DIR, "schemas/IDLs", v, "org.gel.models.ontologies")
+if v == 'latest':
+    outfile = os.path.join(BASE_DIR, "protocols", "GelProtocols.py")
+else:
+    outfile = os.path.join(BASE_DIR, "protocols", "GelProtocols_{version}.py".format(version=module_version))
+ga4gh_outfile = os.path.join(BASE_DIR, "protocols", "GA4GHProtocols.py")
+openCB_outfile = os.path.join(BASE_DIR, "protocols", "openCBProtocols.py")
+cva_outfile = os.path.join(BASE_DIR, "protocols", "CVAProtocols.py")
+ontologies_outfile = os.path.join(BASE_DIR, "protocols", "OntologiesProtocols.py")
 
-print ("version: " + VERSION)
+version = json.load(open(os.path.join(json_folder, "VersionControl", "VersionControl.avsc")))["fields"][0]["default"]
+logging.info("Version: " + version)
+# GeL models Python source generation
+generate_python_sources(schemas, outfile, version)
+# GA4GH models Python source generation
+generate_python_sources(ga4gh_schemas, ga4gh_outfile, version)
+# OpenCB models Python source generation
+generate_python_sources(openCB_schemas, openCB_outfile, version)
+# Ontologies models Python source generation
+generate_python_sources(ontologies_schemas, ontologies_outfile, version)
+# CVA models Python source generation
+generate_python_sources(cva_schemas, cva_outfile, version)
 
 
-os.system("python " + os.path.join(BASE_DIR, "resources", "CodeGenerationFromGA4GH", "process_schemas.py --outputFile "
-                                   + outfile + " --avro-tools-jar " + avro_tools_jar + " --inputSchemasDirectory "
-                                   + schemas + " " + VERSION))
+def generate_documentation(class_name):
+    avrodoc_command = "avrodoc " + os.path.join(avrp_folder, "%s.avpr" % class_name) + " > " + os.path.join(html_folder, "%s.html" % class_name)
+    logging.info("Running: [%s]" % avrodoc_command)
+    os.system(avrodoc_command)
 
-
-os.system("python " + os.path.join(BASE_DIR, "resources", "CodeGenerationFromGA4GH", "process_schemas.py --outputFile "
-                                   + ga4gh_outfile + " --avro-tools-jar " + avro_tools_jar + " --inputSchemasDirectory "
-                                   + ga4gh_schemas + " " + VERSION))
-
-os.system("python " + os.path.join(BASE_DIR, "resources", "CodeGenerationFromGA4GH", "process_schemas.py --outputFile "
-                                   + openCB_outfile + " --avro-tools-jar " + avro_tools_jar + " --inputSchemasDirectory "
-                                   + openCB_schemas + " " + VERSION))
-
-os.system("python " + os.path.join(BASE_DIR, "resources", "CodeGenerationFromGA4GH", "process_schemas.py --outputFile "
-                                   + cva_outfile + " --avro-tools-jar " + avro_tools_jar + " --inputSchemasDirectory "
-                                   + cva_schemas + " " + VERSION))
-
-
-os.system("avrodoc " + os.path.join(avrp_folder, "RDParticipant.avpr") + " > " + os.path.join(html_folder, "RDParticipant.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "ClinicalReportRD.avpr") + " > " + os.path.join(html_folder, "ClinicalReportRD.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "ClinicalReportCancer.avpr") + " > " + os.path.join(html_folder, "ClinicalReportCancer.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "InterpretationRequestRD.avpr") + " > " + os.path.join(html_folder, "RDInterpretationRequests.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "InterpretationRequestCancer.avpr") + " > " + os.path.join(html_folder, "CancerInterpretationRequests.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "InterpretedGenomesRD.avpr") + " > " + os.path.join(html_folder, "RDInterpretedGenomes.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "InterpretedGenomesCancer.avpr") + " > " + os.path.join(html_folder, "CancerInterpretedGenomes.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "CancerParticipant.avpr") + " > " + os.path.join(html_folder, "CancerParticipant.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "GelBamMetrics.avpr") + " > " + os.path.join(html_folder, "GelBamMetrics.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "AuditLog.avpr") + " > " + os.path.join(html_folder, "AuditLog.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "RDParticipantChangeLog.avpr") + " > " + os.path.join(html_folder, "RDParticipantChangeLog.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "MDTDeliveryProtocol.avpr") + " > " + os.path.join(html_folder, "MDTDeliveryProtocol.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "EvidenceSet.avpr") + " > " + os.path.join(html_folder, "EvidenceSet.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "Comment.avpr") + " > " + os.path.join(html_folder, "Comment.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "SequenceOntology.avpr") + " > " + os.path.join(html_folder, "SequenceOntology.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "ReportEventContainer.avpr") + " > " + os.path.join(html_folder, "ReportEventContainer.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "ObservedVariant.avpr") + " > " + os.path.join(html_folder, "ObservedVariant.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "ConfidenceInformationOntology.avpr") + " > " + os.path.join(html_folder, "ConfidenceInformationOntology.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "OntologyTerms.avpr") + " > " + os.path.join(html_folder, "OntologyTerms.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "SupplementaryAnalysisResults.avpr") + " > " + os.path.join(html_folder, "SupplementaryAnalysisResults.html"))
-os.system("avrodoc " + os.path.join(avrp_folder, "ExitQuestionnaire.avpr") + " > " + os.path.join(html_folder, "ExitQuestionnaire.html"))
+# Builds models documentation
+generate_documentation("RDParticipant")
+generate_documentation("ClinicalReportRD")
+generate_documentation("ClinicalReportCancer")
+generate_documentation("InterpretationRequestRD")
+generate_documentation("InterpretationRequestCancer")
+generate_documentation("InterpretedGenomesRD")
+generate_documentation("InterpretedGenomesCancer")
+generate_documentation("CancerParticipant")
+generate_documentation("GelBamMetrics")
+generate_documentation("AuditLog")
+generate_documentation("RDParticipantChangeLog")
+generate_documentation("MDTDeliveryProtocol")
+generate_documentation("EvidenceSet")
+generate_documentation("Comment")
+generate_documentation("SequenceOntology")
+generate_documentation("ReportEventContainer")
+generate_documentation("ObservedVariant")
+generate_documentation("ConfidenceInformationOntology")
+generate_documentation("OntologyTerms")
+generate_documentation("SupplementaryAnalysisResults")
+generate_documentation("ExitQuestionnaire")
