e4b1aed79dd38a6a5fad4c3241a5ae39bddd6d45
Greg Lever <greg.lever@genomicsengland.co.uk>
2017-07-03 16:45:51 +0100

    Feature - Additional Test Functionality * add get_valid_empty_interpreted_genome_rd_3_0_0 * add get_valid_rd_exit_questionnaire_3_0_0 * add test_create_cancer_interpretation_request * add get_valid_clinical_report_cancer_3_0_0 * add get_valid_cancer_sample_3_0_0

--- protocols/tests/test_migration/test_migration_reports_3_0_0_to_participant_1_0_0.py
+++ protocols/tests/test_migration/test_migration_reports_3_0_0_to_participant_1_0_0.py
@@ -1,46 +1,26 @@
 from copy import deepcopy
 from unittest import TestCase
 
-from protocols.migration.migration_reports_3_0_0_to_participant_1_0_0 import MigrateReports3ToParticipant1
-from protocols.participant_1_0_0 import CancerParticipant as CancerParticipant_new
+from protocols.tests import get_valid_cancer_participant_3_0_0
+from protocols.tests import get_valid_empty_cancer_participant_1_0_0
 from protocols.reports_3_0_0 import CancerParticipant as CancerParticipant_old
-from protocols.tests import MockTestObject
+from protocols.participant_1_0_0 import CancerParticipant as CancerParticipant_new
+from protocols.migration.migration_reports_3_0_0_to_participant_1_0_0 import MigrateReports3ToParticipant1
 
 
 class TestMigrateReports3ToParticipant1(TestCase):
 
     def test_migrate_cancer_participant(self):
 
-        old_participant = MockTestObject(object_type=CancerParticipant_old).get_valid_empty_object()
-        old_participant.cancerDemographics.sex = 'M'
-        old_participant.cancerSamples[0].sampleType = 'tumor'
-        old_participant.cancerSamples[0].labId = '1'
-        additional_sample = deepcopy(old_participant.cancerSamples[0])
-        old_participant.cancerSamples.append(additional_sample)
-        old_participant.cancerSamples[1].sampleType = 'germline'
-        old_participant.cancerSamples[1].labId = '2'
-
-        new_participant = MockTestObject(object_type=CancerParticipant_new).get_valid_empty_object()
-        new_participant.sex = 'M'
-        new_participant.germlineSamples.labSampleId = 1
-        new_participant.tumourSamples.tumourId = 1
-        new_participant.tumourSamples.labSampleId = 1
-        new_participant.readyForAnalysis = True
-        new_participant.sex = 'UNKNOWN'
-
-        matchedSample = new_participant.matchedSamples
-        new_participant.matchedSamples = [matchedSample]
-
-        germlineSample = new_participant.germlineSamples
-        new_participant.germlineSamples = [germlineSample]
-
-        tumourSample = new_participant.tumourSamples
-        new_participant.tumourSamples = [tumourSample]
+        old_participant = get_valid_cancer_participant_3_0_0()
+        new_participant = get_valid_empty_cancer_participant_1_0_0()
 
         # Check old_participant is a valid reports_3_0_0 CancerParticipant object
+        self.assertTrue(isinstance(old_participant, CancerParticipant_old))
         self.assertTrue(old_participant.validate(jsonDict=old_participant.toJsonDict()))
 
         # Check new_participant is a valid participant_1_0_0 CancerParticipant object
+        self.assertTrue(isinstance(new_participant, CancerParticipant_new))
         self.assertTrue(new_participant.validate(jsonDict=new_participant.toJsonDict()))
 
         # Perform the migration of old_participant from reports_3_0_0 to participant_1_0_0
