b1719f377860cb9dcee0c6a65d3bc2fe6b59e096
Greg Lever <greg.lever@genomicsengland.co.uk>
2017-09-13 14:13:31 +0100

    remove pprint line

--- protocols/util/generate_mock_objects.py
+++ protocols/util/generate_mock_objects.py
@@ -138,9 +138,6 @@ def validate_object(object_to_validate, object_type):
     if object_to_validate.validate(jsonDict=object_to_validate.toJsonDict()):
         return object_to_validate
     else:
-        from pprint import pprint
-        from protocols.util import handle_avro_errors
-        pprint(handle_avro_errors(object_to_validate.validate_parts()))
         raise Exception("New {object_type} object is not valid".format(object_type=object_type))
 
 
