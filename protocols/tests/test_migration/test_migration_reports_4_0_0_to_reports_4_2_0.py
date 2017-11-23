511af1757b7a562702b58e48db607d9e6fba0c0d
Greg Lever <greg.lever@genomicsengland.co.uk>
2017-11-23 08:37:25 +0000

    HOTFIX - Cancer Clincal Report 4.0.0 to 4.2.0 migration

--- protocols/tests/test_migration/test_migration_reports_4_0_0_to_reports_4_2_0.py
+++ protocols/tests/test_migration/test_migration_reports_4_0_0_to_reports_4_2_0.py
@@ -4,6 +4,7 @@ from protocols import reports_4_0_0
 from protocols import reports_4_2_0
 from protocols.migration import MigrateReports400To420
 from protocols.util.dependency_manager import VERSION_400
+from protocols.util.dependency_manager import VERSION_500
 from protocols.util.factories.avro_factory import GenericFactoryAvro
 
 
@@ -11,6 +12,29 @@ class TestMigrateReports4To420(TestCase):
 
     old_model = reports_4_0_0
     new_model = reports_4_2_0
+
+    def test_migrate_cancer_clinical_report(self):
+
+        cr_c_400 = GenericFactoryAvro.get_factory_avro(
+            self.old_model.ClinicalReportCancer, VERSION_400, fill_nullables=True
+        ).create()
+        self.assertTrue(cr_c_400.validate(cr_c_400.toJsonDict()))
+
+        cr_c_420 = GenericFactoryAvro.get_factory_avro(
+            self.new_model.ClinicalReportCancer, VERSION_500
+        ).create()
+        self.assertTrue(cr_c_420.validate(cr_c_420.toJsonDict()))
+
+        migrated_cir_420 = MigrateReports400To420().migrate_cancer_clinical_report(
+            cancer_clinical_report=cr_c_400, sample_id='test_sample_id'
+        )
+
+        self.assertTrue(migrated_cir_420.validate(migrated_cir_420.toJsonDict()))
+
+        for cv_400, cv_420 in zip(cr_c_400.candidateVariants, migrated_cir_420.candidateVariants):
+            self.assertEqual(cv_400.reportedVariantCancer.reference, cv_420.reference)
+            self.assertEqual(cv_400.reportedVariantCancer.alternate, cv_420.alternate)
+            self.assertEqual(cv_400.reportedVariantCancer.position, cv_420.position)
 
     def test_migrate_cir_400_to_420(self):
 
