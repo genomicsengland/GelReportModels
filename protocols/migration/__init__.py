887ef9799864872a0aa3f64db17559ad9e00261a
priesgo <priesgoferreiro@gmail.com>
2018-05-03 09:13:06 +0100

    remove backwards migrations

--- protocols/migration/__init__.py
+++ protocols/migration/__init__.py
@@ -2,7 +2,6 @@ from .migration import Migration2_1To3
 
 from .base_migration import BaseMigration
 
-from .participants import MigrationParticipants103To100
 from .participants import MigrationParticipants100To103
 from .participants import MigrationParticipants103To110
 
