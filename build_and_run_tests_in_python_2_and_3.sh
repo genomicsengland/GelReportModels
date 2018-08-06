0e52b61b57c3af2f3cd0f8c5a71e1610c5c11ac9
Kevin Savage <kevinpetersavage@users.noreply.github.com>
2018-08-06 13:50:44 +0100

    Make tests pass in both python 2 and 3

--- build_and_run_tests_in_python_2_and_3.sh
+++ build_and_run_tests_in_python_2_and_3.sh
@@ -2,8 +2,6 @@
 docker build -t gelreportmodels:python2 -f Dockerfile-python2 .
 docker build -t gelreportmodels:python3 -f Dockerfile-python3 .
 
-docker run -v ${PWD}:/code -w /code gelreportmodels:python3 'python3 build.py  --skip-docs --skip-java'
-
-docker run -v ${PWD}:/code -w /code gelreportmodels:python3 'python3 -m unittest discover'
-docker run -v ${PWD}:/code -w /code gelreportmodels:python2 'python -m unittest discover'
-
+docker run -v ${PWD}:/code -w /code gelreportmodels:python3 python3 build.py --skip-docs --skip-java
+docker run -v ${PWD}:/code -w /code gelreportmodels:python3 python3 -m unittest discover .
+docker run -v ${PWD}:/code -w /code gelreportmodels:python2 python -m unittest discover .
