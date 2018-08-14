f48cdc861beb9059e84fffdd57f3b612f061e052
Kevin Savage <kevin.savage@diamond.ac.uk>
2018-08-14 17:33:46 +0100

    Revert "Manually copy pablos change to setup.py"
    This reverts commit 4e5da2e1ec66c6911584e9e75c748e4074a71230.

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import os
-import sys
 from setuptools import find_packages, setup
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -15,15 +14,11 @@ reqs = [
     "future==0.16.0"
 ]
 
-enforced_version = os.environ.get("GEL_REPORT_MODELS_PYTHON_VERSION", None)
-interpreter_version = sys.version_info[0]
-target_version = enforced_version if enforced_version else interpreter_version
-if target_version == '2':
+if os.environ["GEL_REPORT_MODELS_PYTHON_VERSION"] == '2':
     reqs += ["avro==1.7.7"]
-elif target_version == '3':
-    reqs += ["avro-python3==1.8.2"]
 else:
-    raise ValueError("Not supported python version {}".format(target_version))
+    reqs += ["avro-python3==1.8.2"]
+
 
 VERSION = "7.1.4"
 setup(
@@ -38,3 +33,4 @@ setup(
     author_email='antonio.rueda-martin@genomicsengland.co.uk',
     description='Bioinformatics team model definitions',
     install_requires=reqs
+)
