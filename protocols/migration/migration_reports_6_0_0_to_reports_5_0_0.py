75bc2756ff92a75069824f5ac9fa695b7df8a1ae
Kevin Savage <kevinpetersavage@users.noreply.github.com>
2018-08-22 10:06:27 +0100

    Mogration working but needs more tests

--- protocols/migration/migration_reports_6_0_0_to_reports_5_0_0.py
+++ protocols/migration/migration_reports_6_0_0_to_reports_5_0_0.py
@@ -1,12 +1,9 @@
 from protocols import reports_6_0_0
 from protocols import reports_5_0_0
-from protocols.migration.base_migration import (
-    BaseMigration,
-    MigrationError,
-)
+from protocols.migration.base_migration_reports_5_0_0_and_reports_6_0_0 import BaseMigrateReports500And600
 
 
-class MigrateReports600To500(BaseMigration):
+class MigrateReports600To500(BaseMigrateReports500And600):
 
     old_model = reports_6_0_0
     new_model = reports_5_0_0
@@ -34,3 +31,93 @@ class MigrateReports600To500(BaseMigration):
         return self.validate_object(
             object_to_validate=new_instance, object_type=self.new_model.CancerInterpretationRequest
         )
+
+    def migrate_cancer_interpreted_genome(self, old_instance):
+        new_instance = self.convert_class(target_klass=self.new_model.CancerInterpretedGenome, instance=old_instance)
+        new_instance.versionControl = self.new_model.ReportVersionControl()
+        new_instance.variants = self.migrate_variants_cancer(variants=old_instance.variants)
+
+        return self.validate_object(object_to_validate=new_instance, object_type=self.new_model.CancerInterpretedGenome)
+
+    def migrate_variants_cancer(self, variants):
+        if variants:
+            return [self.migrate_variant_cancer(old_variant) for old_variant in variants]
+
+    def migrate_variant_cancer(self, old_variant):
+        new_variant = self.convert_class(self.new_model.ReportedVariantCancer, old_variant)
+        attributes = old_variant.variantAttributes.toJsonDict()
+        new_variant.updateWithJsonDict(attributes)
+        new_variant.variantAttributes.fdp50 = str(old_variant.variantAttributes.fdp50)
+
+        identifiers = old_variant.variantAttributes.variantIdentifiers.toJsonDict()
+        new_variant.updateWithJsonDict(identifiers)
+
+        new_variant.variantCalls = [self.migrate_variant_call_cancer(call) for call in old_variant.variantCalls]
+        new_variant.reportEvents = [self.migrate_report_event_cancer(reportEvent) for reportEvent in old_variant.reportEvents]
+
+        return new_variant
+
+    def migrate_variant_call_cancer(self, old_variant_call):
+        new_variant_call = self.convert_class(self.new_model.VariantCall, old_variant_call)
+        if old_variant_call.phaseGenotype:
+            new_variant_call.phaseSet = old_variant_call.phaseGenotype.phaseSet
+        new_variant_call.vaf = old_variant_call.sampleVariantAlleleFrequency
+        return self.validate_object(
+            object_to_validate=new_variant_call, object_type=self.new_model.VariantCall
+        )
+
+    def migrate_report_event_cancer(self, old_report_event):
+        new_event = self.convert_class(target_klass=self.new_model.ReportEventCancer, instance=old_report_event)
+
+        new_event.genomicEntities = [self.migrate_genomic_entity(entity) for entity in old_report_event.genomicEntities]
+        new_event.variantClassification = self.migrate_variant_classification(
+            classification=old_report_event.variantClassification)
+
+        if old_report_event.domain:
+            new_event.tier = self.domain_tier_map[old_report_event.domain]
+        new_event.actions = self.migrate_actions(old_report_event.actions)
+
+        return self.validate_object(object_to_validate=new_event, object_type=self.new_model.ReportEvent)
+
+    def migrate_variant_classification(self, classification):
+        if classification is None:
+            return None
+        new_variant_classification = self.convert_class(self.new_model.VariantClassification, classification)
+        new_variant_classification.clinicalSignificance = self.migrate_clinical_significance(
+            old_significance=classification.clinicalSignificance)
+        new_variant_classification.drugResponseClassification = None
+        return self.validate_object(object_to_validate=new_variant_classification, object_type=self.new_model.VariantClassification)
+
+    def migrate_clinical_significance(self, old_significance):
+        return self.clinical_signicance_reverse_map.get(old_significance)
+
+    def migrate_genomic_entity(self, genomic_entity):
+        new_genomic_entity = self.convert_class(self.new_model.GenomicEntity, genomic_entity)
+        if genomic_entity.otherIds is not None:
+            new_genomic_entity.otherIds = \
+                {identifier.source: identifier.identifier for identifier in genomic_entity.otherIds}
+        return self.validate_object(object_to_validate=new_genomic_entity, object_type=self.new_model.GenomicEntity)
+
+    def migrate_actions(self, actions):
+        if not actions:
+            return []
+        evidences = [("Trial", actions.trials), ("Prognostic", actions.prognosis), ("Therapeutic", actions.therapies)]
+        present_evidences = [(typ, evidence) for typ, evidence in evidences if evidence]
+        return [self._make_action_from(e, typ) for typ, evidence in present_evidences for e in evidence]
+
+    def _make_action_from(self, evidence, evidenceType):
+        action = self.new_model.Action()
+        action.evidenceType = evidenceType
+        if hasattr(evidence, 'source'):
+            action.source = evidence.source
+        else:
+            action.source = "None"
+        if hasattr(evidence, 'references'):
+            action.references = evidence.references
+        if hasattr(evidence, 'studyUrl'):
+            action.url = evidence.studyUrl
+        if hasattr(evidence, 'referenceUrl'):
+            action.url = evidence.referenceUrl
+        action.variantActionable = evidence.variantActionable
+        action.evidenceType += ",".join([' ('] + evidence.conditions + [')'])
+        return action
--- protocols/tests/test_migration/test_round_trip_reports_5_0_0_to_reports_6_0_0.py
+++ protocols/tests/test_migration/test_round_trip_reports_5_0_0_to_reports_6_0_0.py
@@ -21,6 +21,40 @@ class TestRoundTripMigrateReports500To600(TestCaseMigration):
     def test_migrate_interpretation_request_cancer_with_nulls(self):
         self._check_rd(fill_nullables=False)
 
+    def test_migrate_interpretated_genome_cancer(self):
+        self._check_round_trip_migration(
+            MigrateReports500To600().migrate_cancer_interpreted_genome,
+            MigrateReports600To500().migrate_cancer_interpreted_genome,
+            self.old_model.CancerInterpretedGenome,
+            self.new_model.InterpretedGenome,
+            fill_nullables=True,
+            expect_equality=False
+        )
+
+    def test_migrate_reported_variant_cancer(self):
+        self._check_round_trip_migration(
+            MigrateReports500To600().migrate_variant_cancer,
+            MigrateReports600To500().migrate_variant_cancer,
+            self.old_model.ReportedVariantCancer,
+            self.new_model.SmallVariant,
+            fill_nullables=True,
+            expect_equality=False
+        )
+
+    def test_migrate_report_event_cancer_action(self):
+        self._check_round_trip_migration(
+            self._migrate_action,
+            lambda actions: MigrateReports600To500().migrate_actions(actions)[0],
+            self.old_model.Action,
+            self.new_model.Actions,
+            fill_nullables=True,
+            expect_equality=False
+        )
+
+    def _migrate_action(self, action):
+        action.evidenceType = 'Trial (with, some, words)'
+        return MigrateReports500To600().migrate_actions([action])
+
     def _check_cancer(self, fill_nullables):
         self._check_round_trip_migration(
             MigrateReports500To600().migrate_interpretation_request_cancer,
@@ -39,17 +73,27 @@ class TestRoundTripMigrateReports500To600(TestCaseMigration):
             fill_nullables=fill_nullables
         )
 
-    def _check_round_trip_migration(self, forward, backward, original_type, new_type, fill_nullables):
+    def _check_round_trip_migration(self, forward, backward, original_type, new_type,
+                                    fill_nullables, expect_equality=True):
         original = self.get_valid_object(
             object_type=original_type, version=self.version_6_1, fill_nullables=fill_nullables)
 
         original.versionControl = self.old_model.ReportVersionControl()  # to set the right default
 
-        migrated = forward(old_instance=original)
+        migrated = forward(original)
         self.assertIsInstance(migrated, new_type)
         self.assertTrue(migrated.validate(migrated.toJsonDict()))
 
-        round_tripped = backward(old_instance=migrated)
+        round_tripped = backward(migrated)
         self.assertIsInstance(round_tripped, original_type)
-        self.assertTrue(round_tripped.validate(migrated.toJsonDict()))
-        self.assertEqual(round_tripped.toJsonDict(), original.toJsonDict())
+        self.assertValid(round_tripped)
+
+        if expect_equality:
+            self.assertEqual(round_tripped.toJsonDict(), original.toJsonDict())
+
+    def assertValid(self, instance):
+        validation = instance.validate(instance.toJsonDict(), verbose=True)
+        if not validation.result:
+            for message in validation.messages:
+                print(message)
+            self.assertFalse(True)
