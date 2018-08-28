d2b059f2b5e9dd71270264fa7423c231f5c82061
Kevin Savage <kevinpetersavage@users.noreply.github.com>
2018-08-28 09:38:29 +0100

    Migrate interpreted genome instead of request

--- protocols/migration/migration_helpers.py
+++ protocols/migration/migration_helpers.py
@@ -139,7 +139,7 @@ class MigrationHelpers(object):
             lambda x: MigrateReports400To500().migrate_interpreted_genome_rd(
                 x, assembly=assembly, interpretation_request_version=interpretation_request_version),
             MigrateReports3To4().migrate_interpreted_genome_rd,
-            Migration2_1To3().migrate_interpretation_request
+            Migration2_1To3().migrate_interpreted_genome
         ]
 
         return MigrationHelpers.migrate(json_dict, types, migrations)
