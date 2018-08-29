82e8434f08e22fe66d3e9c3610769153c63d3933
Kevin Savage <kevinpetersavage@users.noreply.github.com>
2018-08-29 14:30:43 +0100

    Second attempt at merging reverse_migrate_interpretation_request_cancer_to_v4

--- protocols/migration/migration_reports_6_0_0_to_reports_5_0_0.py
+++ protocols/migration/migration_reports_6_0_0_to_reports_5_0_0.py
@@ -126,6 +126,9 @@ class MigrateReports600To500(BaseMigrateReports500And600):
         new_instance.variantCalls = self.convert_collection(small_variant.variantCalls, self.migrate_variant_call)
         new_instance.reportEvents = self.convert_collection(small_variant.reportEvents, self.migrate_report_event)
 
+        if new_instance.alleleOrigins is None:
+            new_instance.alleleOrigins = []
+
         return self.validate_object(object_to_validate=new_instance, object_type=self.new_model.ReportedVariant)
 
     def migrate_variant_attributes(self, old_variant_attributes):
@@ -243,12 +246,19 @@ class MigrateReports600To500(BaseMigrateReports500And600):
 
     def migrate_variant_cancer(self, old_variant):
         new_variant = self.convert_class(self.new_model.ReportedVariantCancer, old_variant)
-        attributes = old_variant.variantAttributes.toJsonDict()
-        new_variant.updateWithJsonDict(attributes)
-        new_variant.variantAttributes.fdp50 = str(old_variant.variantAttributes.fdp50)
-        new_variant.updateWithJsonDict(old_variant.variantAttributes.variantIdentifiers.toJsonDict())
-        new_variant.variantCalls = self.convert_collection(old_variant.variantCalls, self.migrate_variant_call_cancer)
-        new_variant.reportEvents = self.convert_collection(old_variant.reportEvents, self.migrate_report_event_cancer)
+        if old_variant.variantAttributes:
+            attributes = old_variant.variantAttributes.toJsonDict()
+            new_variant.updateWithJsonDict(attributes)
+            new_variant.variantAttributes.fdp50 = str(old_variant.variantAttributes.fdp50)
+            if old_variant.variantAttributes.variantIdentifiers:
+                identifiers = old_variant.variantAttributes.variantIdentifiers.toJsonDict()
+                new_variant.updateWithJsonDict(identifiers)
+
+        new_variant.variantCalls = [self.migrate_variant_call_cancer(call) for call in old_variant.variantCalls]
+        new_variant.reportEvents = [self.migrate_report_event_cancer(reportEvent) for reportEvent in old_variant.reportEvents]
+
+        if new_variant.alleleOrigins is None:
+            new_variant.alleleOrigins = []
 
         return new_variant
 
@@ -257,6 +267,8 @@ class MigrateReports600To500(BaseMigrateReports500And600):
         if old_variant_call.phaseGenotype:
             new_variant_call.phaseSet = old_variant_call.phaseGenotype.phaseSet
         new_variant_call.vaf = old_variant_call.sampleVariantAlleleFrequency
+        if new_variant_call.alleleOrigins is None:
+            new_variant_call.alleleOrigins = []
         return self.validate_object(
             object_to_validate=new_variant_call, object_type=self.new_model.VariantCall
         )
@@ -295,6 +307,7 @@ class MigrateReports600To500(BaseMigrateReports500And600):
 
     def _make_action_from(self, evidence, evidenceType):
         action = self.new_model.Action()
+        action.actionType = {"Trial": "therapy", "Therapeutic": "therapeutic", "Prognostic": "prognosis"}[evidenceType]
         action.evidenceType = evidenceType
         if hasattr(evidence, 'source'):
             action.source = evidence.source
