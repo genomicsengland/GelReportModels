dc7e0189d2cba04a5879e92bfd2e419a9052c6e0
priesgo <priesgoferreiro@gmail.com>
2018-09-04 14:55:29 +0100

    merge setup.py properly!

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ from setuptools import find_packages, setup
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-
 reqs = [
     "Sphinx==1.6.2",
     "sphinx_rtd_theme==0.2.4",
-    "avro==1.7.7",
     "factory-boy==2.9.2",
     "humanize==0.5.1",
     "PyYAML==3.12",
     "ujson==1.35",
-    "dictdiffer"
+    "dictdiffer",
+    "future==0.16.0"
 ]
 
 enforced_version = os.environ.get("GEL_REPORT_MODELS_PYTHON_VERSION", None)
