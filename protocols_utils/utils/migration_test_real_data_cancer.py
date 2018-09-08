4e8f86b5f7707577e7f435603202b7be8b3bb233
priesgo <priesgoferreiro@gmail.com>
2018-09-08 11:17:22 +0100

    refactor tests on real data

--- protocols_utils/utils/migration_test_real_data_cancer.py
+++ protocols_utils/utils/migration_test_real_data_cancer.py
@@ -1,27 +1,15 @@
 #!/usr/bin/env python
-from pycipapi.cipapi_client import CipApiClient, CipApiCase
-import getpass
+from protocols_utils.utils.abstract_migration_test_real_data import AbstractRealRoundTripper
 from protocols import reports_4_0_0, reports_5_0_0, reports_6_0_0
 from protocols.migration.base_migration import MigrationError
 from protocols.reports_6_0_0 import Program
-from protocols.tests.test_migration.migration_runner import MigrationRunner
 from itertools import chain
-import logging
 
 
-class RealRoundTripperCancer(object):
+class RealRoundTripperCancer(AbstractRealRoundTripper):
 
     def __init__(self):
-        # configures logging to get only logs about the failed cases
-        self.log_file = "real_data_cancer_round_trip.log"
-        logging.basicConfig(filename=self.log_file, level=logging.ERROR,
-                            format='%(asctime)s.%(msecs)03d - %(message)s',
-                            datefmt='%H:%M:%S', )
-        gel_user = raw_input("User:")
-        gel_password = getpass.getpass("Password:")
-        cipapi_url = raw_input("URL:")
-        self.cipapi_client = CipApiClient(cipapi_url, user=gel_user, password=gel_password, retries=-1)
-        self.migration_runner = MigrationRunner()
+        super(RealRoundTripperCancer, self).__init__("real_data_cancer_round_trip.log", Program.cancer, "data")
 
     def _check_actions(self, original_reported_variants, round_tripped_reported_variants):
         expected_report_events = chain.from_iterable(
@@ -33,85 +21,72 @@ class RealRoundTripperCancer(object):
     def process_case(self, case):
         # interpretation request
         raw_ir, case_id, version = case._get_raw_interpretation_request()
-        ir = reports_4_0_0.CancerInterpretationRequest.fromJsonDict(
-            raw_ir)  # type: reports_4_0_0.CancerInterpretationRequest
+        ir = reports_4_0_0.CancerInterpretationRequest.fromJsonDict(raw_ir)
         try:
             ir_migrated, ir_round_tripped = self.migration_runner.roundtrip_cancer_ir(ir, case.assembly)
+            is_valid = ir_migrated.validate(reports_6_0_0.CancerInterpretationRequest, ir_migrated.toJsonDict())
+            is_valid_round_tripped = ir_round_tripped.validate(
+                reports_4_0_0.CancerInterpretationRequest, ir_round_tripped.toJsonDict())
+            differ = self.migration_runner.diff_round_tripped(
+                ir, ir_round_tripped,
+                ignore_fields=["TNMStageVersion", "TNMStageGrouping",
+                               "additionalTextualVariantAnnotations", "matchedSamples", "commonAf",
+                               "actions",  # NOTE: this is ignores as actions come back in a different order
+                               "versionControl",  # NOTE: this is ignored as some real data have a missing version value
+                               "additionalInfo"
+                               ])
+            differ |= self._check_actions(ir.tieredVariants, ir_round_tripped.tieredVariants)
+            RealRoundTripperCancer.log_case("IR", case_id, version, '4.0.0', differ, is_valid, is_valid_round_tripped)
         except MigrationError as e:
-            logging.error("Skipping case {}-{}: {}".format(case_id, version, e.message))
-            return
-        is_valid = ir_migrated.validate(reports_6_0_0.CancerInterpretationRequest, ir_migrated.toJsonDict())
-        is_valid_round_tripped = ir_round_tripped.validate(
-            reports_4_0_0.CancerInterpretationRequest, ir_round_tripped.toJsonDict())
-        differ = self.migration_runner.diff_round_tripped(
-            ir, ir_round_tripped,
-            ignore_fields=["TNMStageVersion", "TNMStageGrouping",
-                           "additionalTextualVariantAnnotations", "matchedSamples", "commonAf",
-                           "actions",  # NOTE: this is ignores as actions come back in a different order
-                           "versionControl",  # NOTE: this is ignored as some real data have a missing version value
-                           "additionalInfo"
-                           ])
-        differ |= self._check_actions(ir.tieredVariants, ir_round_tripped.tieredVariants)
-
-        if not is_valid:
-            logging.error("Invalid cancer IR {}-{}".format(case_id, version))
-        if not is_valid_round_tripped:
-            logging.error("Invalid round tripped cancer IR {}-{}".format(case_id, version))
-        logging.error("{} cancer IR {}-{}".format("KO" if differ else "OK", case_id, version))
+            RealRoundTripperCancer.log("MIGRATION_ERROR", "IR", '4.0.0', case_id, version, e.message)
 
         # interpreted genome
         if case.has_interpreted_genome():
             ig = reports_4_0_0.CancerInterpretedGenome.fromJsonDict(
                 case.raw_interpreted_genome)  # type: reports_4_0_0.CancerInterpretedGenome
-            ig_migrated, ig_round_tripped = self.migration_runner.roundtrip_cancer_ig(ig, case.assembly)
-            is_valid = ig_migrated.validate(reports_6_0_0.InterpretedGenome, ig_migrated.toJsonDict())
-            is_valid_round_tripped = ig_round_tripped.validate(
-                reports_4_0_0.CancerInterpretedGenome, ig_round_tripped.toJsonDict())
-            differ = self.migration_runner.diff_round_tripped(
-                ig, ig_round_tripped, ignore_fields=["analysisId", "additionalTextualVariantAnnotations", "commonAf"])
-            differ |= self._check_actions(ig.reportedVariants, ig_round_tripped.reportedVariants)
-            if not is_valid:
-                logging.error("Invalid cancer IG {}-{}".format(case_id, version))
-            if not is_valid_round_tripped:
-                logging.error("Invalid round tripped cancer IG {}-{}".format(case_id, version))
-            logging.error("{} cancer IG {}-{}".format("KO" if differ else "OK", case_id, version))
+            try:
+                ig_migrated, ig_round_tripped = self.migration_runner.roundtrip_cancer_ig(ig, case.assembly)
+                is_valid = ig_migrated.validate(reports_6_0_0.InterpretedGenome, ig_migrated.toJsonDict())
+                is_valid_round_tripped = ig_round_tripped.validate(
+                    reports_4_0_0.CancerInterpretedGenome, ig_round_tripped.toJsonDict())
+                differ = self.migration_runner.diff_round_tripped(
+                    ig, ig_round_tripped, ignore_fields=["analysisId", "additionalTextualVariantAnnotations", "commonAf"])
+                differ |= self._check_actions(ig.reportedVariants, ig_round_tripped.reportedVariants)
+                RealRoundTripperCancer.log_case("IG", case_id, version, '4.0.0', differ, is_valid, is_valid_round_tripped)
+            except MigrationError as e:
+                RealRoundTripperCancer.log("MIGRATION_ERROR", "IG", '4.0.0', case_id, version, e.message)
 
         # clinical report
         if case.has_clinical_report():
             cr = reports_4_0_0.ClinicalReportCancer.fromJsonDict(
                 case.raw_clinical_report)  # type: reports_4_0_0.ClinicalReportCancer
-            cr_migrated, cr_round_tripped = self.migration_runner.roundtrip_cancer_cr(cr, case.assembly)
-            is_valid = cr_migrated.validate(reports_6_0_0.ClinicalReport, cr_migrated.toJsonDict())
-            is_valid_round_tripped = cr_round_tripped.validate(
-                reports_4_0_0.ClinicalReportCancer, cr_round_tripped.toJsonDict())
-            differ = self.migration_runner.diff_round_tripped(
-                cr, cr_round_tripped, ignore_fields=["analysisId", "additionalTextualVariantAnnotations", "commonAf",
-                                                     "genePanelsCoverage", "actions"])
-            differ |= self._check_actions(cr.candidateVariants, cr_round_tripped.candidateVariants)
-            if not is_valid:
-                logging.error("Invalid cancer CR {}-{}".format(case_id, version))
-            if not is_valid_round_tripped:
-                logging.error("Invalid round tripped cancer CR {}-{}".format(case_id, version))
-            logging.error("{} cancer CR {}-{}".format("KO" if differ else "OK", case_id, version))
+            try:
+                cr_migrated, cr_round_tripped = self.migration_runner.roundtrip_cancer_cr(cr, case.assembly)
+                is_valid = cr_migrated.validate(reports_6_0_0.ClinicalReport, cr_migrated.toJsonDict())
+                is_valid_round_tripped = cr_round_tripped.validate(
+                    reports_4_0_0.ClinicalReportCancer, cr_round_tripped.toJsonDict())
+                differ = self.migration_runner.diff_round_tripped(
+                    cr, cr_round_tripped, ignore_fields=["analysisId", "additionalTextualVariantAnnotations", "commonAf",
+                                                         "genePanelsCoverage", "actions"])
+                differ |= self._check_actions(cr.candidateVariants, cr_round_tripped.candidateVariants)
+                RealRoundTripperCancer.log_case("CR", case_id, version, '4.0.0', differ, is_valid,
+                                                is_valid_round_tripped)
+            except MigrationError as e:
+                RealRoundTripperCancer.log("MIGRATION_ERROR", "CR", '4.0.0', case_id, version, e.message)
 
         if case.has_exit_questionnaire():
             eq = reports_5_0_0.CancerExitQuestionnaire.fromJsonDict(case.raw_questionnaire)
-            eq_migrated, eq_round_tripped = self.migration_runner.roundtrip_cancer_eq(eq, case.assembly)
-            is_valid = eq_migrated.validate(reports_6_0_0.CancerExitQuestionnaire, eq_migrated.toJsonDict())
-            is_valid_round_tripped = eq_round_tripped.validate(
-                reports_5_0_0.CancerExitQuestionnaire, eq_round_tripped.toJsonDict())
-            differ = self.migration_runner.diff_round_tripped(
-                eq, eq_round_tripped, ignore_fields=[])
-            if not is_valid:
-                logging.error("Invalid cancer EQ {}-{}".format(case_id, version))
-            if not is_valid_round_tripped:
-                logging.error("Invalid round tripped cancer EQ {}-{}".format(case_id, version))
-            logging.error("{} cancer EQ {}-{}".format("KO" if differ else "OK", case_id, version))
-
-    def run(self):
-        print "Check your results in '{}'".format(self.log_file)
-        for case in self.cipapi_client.get_cases(program=Program.cancer):  # type: CipApiCase
-            self.process_case(case)
+            try:
+                eq_migrated, eq_round_tripped = self.migration_runner.roundtrip_cancer_eq(eq, case.assembly)
+                is_valid = eq_migrated.validate(reports_6_0_0.CancerExitQuestionnaire, eq_migrated.toJsonDict())
+                is_valid_round_tripped = eq_round_tripped.validate(
+                    reports_5_0_0.CancerExitQuestionnaire, eq_round_tripped.toJsonDict())
+                differ = self.migration_runner.diff_round_tripped(
+                    eq, eq_round_tripped, ignore_fields=[])
+                RealRoundTripperCancer.log_case("EQ", case_id, version, '5.0.0', differ, is_valid,
+                                                is_valid_round_tripped)
+            except MigrationError as e:
+                RealRoundTripperCancer.log("MIGRATION_ERROR", "EQ", '5.0.0', case_id, version, e.message)
 
 
 if __name__ == '__main__':
--- protocols_utils/utils/migration_test_real_data_rd.py
+++ protocols_utils/utils/migration_test_real_data_rd.py
@@ -1,121 +1,102 @@
 #!/usr/bin/env python
-from pycipapi.cipapi_client import CipApiClient, CipApiCase
-import getpass
 from protocols import reports_2_1_0, reports_3_0_0, reports_6_0_0
 from protocols.migration import Migration21To3
 from protocols.migration.base_migration import MigrationError
 from protocols.reports_6_0_0 import Program
-from protocols.tests.test_migration.migration_runner import MigrationRunner
-import logging
+from protocols_utils.utils.abstract_migration_test_real_data import AbstractRealRoundTripper
 
 
-class RealRoundTripperRd(object):
+class RealRoundTripperRd(AbstractRealRoundTripper):
 
     def __init__(self):
-        # configures logging to get only logs about the failed cases
-        self.log_file = "real_data_rd_round_trip.log"
-        logging.basicConfig(filename=self.log_file, level=logging.ERROR,
-                            format='%(asctime)s.%(msecs)03d - %(message)s',
-                            datefmt='%H:%M:%S',)
-        gel_user = raw_input("User:")
-        gel_password = getpass.getpass("Password:")
-        cipapi_url = raw_input("URL:")
-        self.cipapi_client = CipApiClient(cipapi_url, user=gel_user, password=gel_password, retries=-1)
-        self.migration_runner = MigrationRunner()
+        super(RealRoundTripperRd, self).__init__("real_data_rd_round_trip.log", Program.rare_disease, "data")
 
     def process_case(self, case):
         # interpretation request
         raw_ir, case_id, version = case._get_raw_interpretation_request()
-        ir = reports_2_1_0.InterpretationRequestRD.fromJsonDict(raw_ir)
-        if ir.versionControl.GitVersionControl == '2.1.0':
-            ir = Migration21To3().migrate_interpretation_request(
-                reports_2_1_0.InterpretationRequestRD.fromJsonDict(raw_ir))
-        else:
-            ir = reports_3_0_0.InterpretationRequestRD.fromJsonDict(raw_ir)
-
         try:
+            ir = reports_2_1_0.InterpretationRequestRD.fromJsonDict(raw_ir)
+            model_version = '3.0.0'
+            if ir.versionControl.GitVersionControl == '2.1.0':
+                model_version = '2.1.0'
+                ir = Migration21To3().migrate_interpretation_request(
+                    reports_2_1_0.InterpretationRequestRD.fromJsonDict(raw_ir))
+            else:
+                ir = reports_3_0_0.InterpretationRequestRD.fromJsonDict(raw_ir)
             ir_migrated, ir_round_tripped = self.migration_runner.roundtrip_rd_ir(ir, case.assembly)
+            is_valid = ir_migrated.validate(reports_6_0_0.InterpretationRequestRD, ir_migrated.toJsonDict())
+            is_valid_round_tripped = ir_round_tripped.validate(
+                reports_3_0_0.InterpretationRequestRD, ir_round_tripped.toJsonDict())
+            differ = self.migration_runner.diff_round_tripped(
+                ir, ir_round_tripped,
+                ignore_fields=[
+                    "ageOfOnset", "consanguineousPopulation", "reportUri", "GitVersionControl",
+                    "analysisId", "genomeAssemblyVersion", "modifiers", "additionalInfo"
+                ])
+            RealRoundTripperRd.log_case("IR", case_id, version, model_version, differ, is_valid, is_valid_round_tripped)
         except MigrationError as e:
-            logging.error("Skipping case {}-{}: {}".format(case_id, version, e.message))
-            return
-
-        is_valid = ir_migrated.validate(reports_6_0_0.InterpretationRequestRD, ir_migrated.toJsonDict())
-        is_valid_round_tripped = ir_round_tripped.validate(
-            reports_3_0_0.InterpretationRequestRD, ir_round_tripped.toJsonDict())
-        differ = self.migration_runner.diff_round_tripped(
-            ir, ir_round_tripped,
-            ignore_fields=[
-                "ageOfOnset", "consanguineousPopulation", "reportUri", "GitVersionControl",
-                "analysisId", "genomeAssemblyVersion", "modifiers", "additionalInfo"
-            ])
-
-        if not is_valid:
-            logging.error("Invalid rd IR {}-{}".format(case_id, version))
-        if not is_valid_round_tripped:
-            logging.error("Invalid round tripped rd IR {}-{}".format(case_id, version))
-        logging.error("{} rd IR {}-{}".format("KO" if differ else "OK", case_id, version))
+            RealRoundTripperRd.log("MIGRATION_ERROR", "IR", model_version, case_id, version, e.message)
 
         # interpreted genome
         if case.has_interpreted_genome():
             raw_ig = case.raw_interpreted_genome
-            if reports_2_1_0.InterpretedGenomeRD.validate(
-                    reports_2_1_0.InterpretedGenomeRD, reports_2_1_0.InterpretedGenomeRD.fromJsonDict(raw_ig)):
-                ig = Migration21To3().migrate_interpreted_genome(
-                    reports_2_1_0.InterpretedGenomeRD.fromJsonDict(raw_ig))
-            else:
-                ig = reports_3_0_0.InterpretedGenomeRD.fromJsonDict(raw_ig)  # type: reports_3_0_0.InterpretedGenomeRD
-            ig_migrated, ig_round_tripped = self.migration_runner.roundtrip_rd_ig(ig, case.assembly)
-            is_valid = ig_migrated.validate(reports_6_0_0.InterpretedGenome, ig_migrated.toJsonDict())
-            is_valid_round_tripped = ig_round_tripped.validate(
-                reports_3_0_0.InterpretedGenomeRD, ig_round_tripped.toJsonDict())
-            differ = self.migration_runner.diff_round_tripped(
-                ig, ig_round_tripped, ignore_fields=['additionalNumericVariantAnnotations', "reportURI", "analysisId",
-                                                     "GitVersionControl"])
-            if not is_valid:
-                logging.error("Invalid rd IG {}-{}".format(case_id, version))
-            if not is_valid_round_tripped:
-                logging.error("Invalid round tripped rd IG {}-{}".format(case_id, version))
-            logging.error("{} rd IG {}-{}".format("KO" if differ else "OK", case_id, version))
+            try:
+                ig = reports_2_1_0.InterpretedGenomeRD.fromJsonDict(raw_ir)
+                model_version = '3.0.0'
+                if ig.versionControl.GitVersionControl == '2.1.0':
+                    model_version = '2.1.0'
+                    ig = Migration21To3().migrate_interpreted_genome(ig)
+                else:
+                    ig = reports_3_0_0.InterpretedGenomeRD.fromJsonDict(raw_ig)
+                ig_migrated, ig_round_tripped = self.migration_runner.roundtrip_rd_ig(ig, case.assembly)
+                is_valid = ig_migrated.validate(reports_6_0_0.InterpretedGenome, ig_migrated.toJsonDict())
+                is_valid_round_tripped = ig_round_tripped.validate(
+                    reports_3_0_0.InterpretedGenomeRD, ig_round_tripped.toJsonDict())
+                differ = self.migration_runner.diff_round_tripped(
+                    ig, ig_round_tripped,
+                    ignore_fields=['additionalNumericVariantAnnotations', "reportURI", "analysisId",
+                                   "GitVersionControl"])
+                RealRoundTripperRd.log_case("IG", case_id, version, model_version, differ, is_valid,
+                                            is_valid_round_tripped)
+            except MigrationError as e:
+                RealRoundTripperRd.log("MIGRATION_ERROR", "IG", model_version, case_id, version, e.message)
 
         # clinical report
         if case.has_clinical_report():
             raw_cr = case.raw_clinical_report
-            if reports_2_1_0.ClinicalReportRD.validate(
-                    reports_2_1_0.InterpretedGenomeRD, reports_2_1_0.InterpretedGenomeRD.fromJsonDict(raw_cr)):
-                cr = Migration21To3().migrate_clinical_report(reports_2_1_0.ClinicalReportRD.fromJsonDict(raw_cr))
-            else:
-                cr = reports_3_0_0.ClinicalReportRD.fromJsonDict(raw_cr)  # type: reports_3_0_0.ClinicalReportRD
-            cr_migrated, cr_round_tripped = self.migration_runner.roundtrip_rd_cr(cr, case.assembly)
-            is_valid = cr_migrated.validate(reports_6_0_0.ClinicalReport, cr_migrated.toJsonDict())
-            is_valid_round_tripped = cr_round_tripped.validate(
-                reports_3_0_0.ClinicalReportRD, cr_round_tripped.toJsonDict())
-            differ = self.migration_runner.diff_round_tripped(
-                cr, cr_round_tripped, ignore_fields=["interpretationRequestAnalysisVersion", "GitVersionControl"])
-            if not is_valid:
-                logging.error("Invalid rd CR {}-{}".format(case_id, version))
-            if not is_valid_round_tripped:
-                logging.error("Invalid round tripped rd CR {}-{}".format(case_id, version))
-            logging.error("{} rd CR {}-{}".format("KO" if differ else "OK", case_id, version))
+            try:
+                model_version = '3.0.0'
+                if reports_2_1_0.ClinicalReportRD.validate(
+                        reports_2_1_0.InterpretedGenomeRD, reports_2_1_0.InterpretedGenomeRD.fromJsonDict(raw_cr)):
+                    model_version = '2.1.0'
+                    cr = Migration21To3().migrate_clinical_report(reports_2_1_0.ClinicalReportRD.fromJsonDict(raw_cr))
+                else:
+                    cr = reports_3_0_0.ClinicalReportRD.fromJsonDict(raw_cr)
+                cr_migrated, cr_round_tripped = self.migration_runner.roundtrip_rd_cr(cr, case.assembly)
+                is_valid = cr_migrated.validate(reports_6_0_0.ClinicalReport, cr_migrated.toJsonDict())
+                is_valid_round_tripped = cr_round_tripped.validate(
+                    reports_3_0_0.ClinicalReportRD, cr_round_tripped.toJsonDict())
+                differ = self.migration_runner.diff_round_tripped(
+                    cr, cr_round_tripped, ignore_fields=["interpretationRequestAnalysisVersion", "GitVersionControl"])
+                RealRoundTripperRd.log_case("CR", case_id, version, model_version, differ, is_valid,
+                                            is_valid_round_tripped)
+            except MigrationError as e:
+                RealRoundTripperRd.log("MIGRATION_ERROR", "CR", model_version, case_id, version, e.message)
 
         if case.has_exit_questionnaire():
             raw_eq = case.raw_questionnaire
-            eq = reports_3_0_0.RareDiseaseExitQuestionnaire.fromJsonDict(raw_eq)  # type: reports_3_0_0.RareDiseaseExitQuestionnaire
-            eq_migrated, eq_round_tripped = self.migration_runner.roundtrip_rd_eq(eq, case.assembly)
-            is_valid = eq_migrated.validate(reports_6_0_0.RareDiseaseExitQuestionnaire, eq_migrated.toJsonDict())
-            is_valid_round_tripped = eq_round_tripped.validate(
-                reports_3_0_0.RareDiseaseExitQuestionnaire, eq_round_tripped.toJsonDict())
-            differ = self.migration_runner.diff_round_tripped(
-                eq, eq_round_tripped, ignore_fields=[])
-            if not is_valid:
-                logging.error("Invalid rd EQ {}-{}".format(case_id, version))
-            if not is_valid_round_tripped:
-                logging.error("Invalid round tripped rd EQ {}-{}".format(case_id, version))
-            logging.error("{} rd EQ {}-{}".format("KO" if differ else "OK", case_id, version))
-
-    def run(self):
-        print "Check your results in '{}'".format(self.log_file)
-        for case in self.cipapi_client.get_cases(program=Program.rare_disease):  # type: CipApiCase
-            self.process_case(case)
+            eq = reports_3_0_0.RareDiseaseExitQuestionnaire.fromJsonDict(raw_eq)
+            try:
+                eq_migrated, eq_round_tripped = self.migration_runner.roundtrip_rd_eq(eq, case.assembly)
+                is_valid = eq_migrated.validate(reports_6_0_0.RareDiseaseExitQuestionnaire, eq_migrated.toJsonDict())
+                is_valid_round_tripped = eq_round_tripped.validate(
+                    reports_3_0_0.RareDiseaseExitQuestionnaire, eq_round_tripped.toJsonDict())
+                differ = self.migration_runner.diff_round_tripped(
+                    eq, eq_round_tripped, ignore_fields=[])
+                RealRoundTripperRd.log_case("EQ", case_id, version, '3.0.0', differ, is_valid,
+                                            is_valid_round_tripped)
+            except MigrationError as e:
+                RealRoundTripperRd.log("MIGRATION_ERROR", "EQ", '3.0.0', case_id, version, e.message)
 
 
 if __name__ == '__main__':
