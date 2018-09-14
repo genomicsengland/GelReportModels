00bc190d17524cd783af0b3e8ac77b63b411f10b
Kevin Savage <kevinpetersavage@users.noreply.github.com>
2018-09-14 15:58:43 +0100

    Release 7.1.12

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ elif target_version == '3':
 else:
     raise ValueError("Not supported python version {}".format(target_version))
 
-VERSION = "7.1.11"
+VERSION = "7.1.12"
 setup(
     name='GelReportModels',
     version=VERSION,
