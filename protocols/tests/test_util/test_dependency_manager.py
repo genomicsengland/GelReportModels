61cd5bc0ba08bc6bed4ac7c35a7aa6e8191d47c0
antonior26 <aruemar@gmail.com>
2018-11-01 13:33:04 +0000

    test fixed

--- protocols/tests/test_util/test_dependency_manager.py
+++ protocols/tests/test_util/test_dependency_manager.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from protocols.util.dependency_manager import DependencyManager
-import protocols.reports_6_0_0
+import protocols.reports_6_1_0
 import protocols.reports_5_0_0
 import protocols.reports_4_2_0
 import protocols.reports_4_0_0
@@ -13,7 +13,7 @@ class TestDependencyManager(TestCase):
         assert dependency_manager is not None
         latest_dependencies = dependency_manager.get_latest_version_dependencies()
         assert isinstance(latest_dependencies, dict)
-        assert latest_dependencies["org.gel.models.report.avro"] == protocols.reports_6_0_0
+        assert latest_dependencies["org.gel.models.report.avro"] == protocols.reports_6_1_0
         dependencies_400 = dependency_manager.get_version_dependencies("4.0.0")
         assert isinstance(dependencies_400, dict)
         assert dependencies_400["org.gel.models.report.avro"] == protocols.reports_4_0_0
