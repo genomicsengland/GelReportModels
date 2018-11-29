233dce6882c23faef8ee14990b1db81f653c514b
antonior26 <aruemar@gmail.com>
2018-11-29 13:46:39 +0000

    updated versions

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ elif target_version == '3':
 else:
     raise ValueError("Not supported python version {}".format(target_version))
 
-VERSION = "7.1.16"
+VERSION = "7.2.0"
 setup(
     name='GelReportModels',
     version=VERSION,
