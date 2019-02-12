0a3f91053d307832a1d61e83e4b69ebaac58e059
Antonior26 <antonio.rueda-martin@genomicsengland.co.uk>
2019-02-12 14:03:13 +0000

    More changes to PGX EQ (#442)
    * IP-3042 - This fixed the problem with the migration failing and PGX EQ (#440)
    
    * * Fixed problem with migration in cancer reportEvents
    * Added new options to PGX EQ questions
    
    * Changed version in pom.xml
    
    * removed others
    
    * new changes in EQ for PGX
    
    * v7.2.5

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ elif target_version == '3':
 else:
     raise ValueError("Not supported python version {}".format(target_version))
 
-VERSION = "7.2.4"
+VERSION = "7.2.5"
 setup(
     name='GelReportModels',
     version=VERSION,
