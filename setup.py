b518006948a42860c3c6aff88bd3cfb1ed601ecf
priesgo <priesgoferreiro@gmail.com>
2019-02-28 14:07:31 +0000

    change the version to 7.2.6

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ elif target_version == '3':
 else:
     raise ValueError("Not supported python version {}".format(target_version))
 
-VERSION = "7.2.4"
+VERSION = "7.2.6"
 setup(
     name='GelReportModels',
     version=VERSION,
