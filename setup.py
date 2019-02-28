7291340167c8ea7f5fce7e58a528aa1598441ac0
priesgo <priesgoferreiro@gmail.com>
2019-02-28 13:23:58 +0000

    rollback version to 7.2.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ elif target_version == '3':
 else:
     raise ValueError("Not supported python version {}".format(target_version))
 
-VERSION = "7.3.0"
+VERSION = "7.2.4"
 setup(
     name='GelReportModels',
     version=VERSION,
