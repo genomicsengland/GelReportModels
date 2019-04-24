a77ef2cd38c89c6bf40ba5a91696507158ce71fe
priesgo <priesgoferreiro@gmail.com>
2019-04-24 23:55:41 +0100

    change models not to repeat variants

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ elif target_version == '3':
 else:
     raise ValueError("Not supported python version {}".format(target_version))
 
-VERSION = "7.2.7"
+VERSION = "7.2.9"
 setup(
     name='GelReportModels',
     version=VERSION,
